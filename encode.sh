2f5f80ee2f90de2939a54fbfd3c0b7bbdbcc9d27
0x65-e <16930669+0x65-e@users.noreply.github.com>
2022-07-06 19:06:28 -0700

    Revert base64-encoded static assets
    Add powershell script to upload binary files using new kv:bulk upload method, which permits base64-encoded uploads.

--- encode.sh
+++
@@ -1,5 +0,0 @@
-for file in $(ls *.jpg *.ttf *.woff *.woff2 *.eot)
-do
-	base64 "$file" > "$file".b64
-	mv "$file".b64 "$file"
-done
---
+++ kv-bulk-upload.ps1
@@ -0,0 +1,52 @@
+param (
+	[switch]$Preview,
+	[String]$Binding = "STATIC",
+	[String]$OutFile = "kv-bulk.json",
+	[String]$Base = ".\static\"
+)
+$BaseDir = (Get-Item -Path $Base)
+
+# Remove outfile if it exists
+if (Test-Path $OutFile) {
+	Write-Host "Deleting existing $OutFile"
+	Remove-Item $OutFile
+}
+
+$BinaryExtensions = ".jpg",".ttf",".woff",".woff2",".eot"
+Write-Host "Uploading files from directory $BaseDir"
+Add-Content -Path $OutFile -Value "["
+$PrefixSize = $BaseDir.FullName.Length
+# Get every file in the base directory
+foreach ($File in Get-ChildItem -File -Recurse -Path $BaseDir) { 
+	$SubPath = $File.FullName.substring($PrefixSize)
+	$IsBinary = $BinaryExtensions.Contains($File.Extension)
+	Write-Host "- $SubPath"
+	# This will fail if the file is empty
+	if ($File.length) {
+		if ($IsBinary) {
+			Write-Host "Binary data file. Uploading in bulk."
+			Add-Content -Path $OutFile -Value "`t{`n`t`t`"key`": `"$($SubPath.Replace('\','/'))`","
+			$EncodedContents = [convert]::ToBase64String((Get-Content -Path $File.FullName -Encoding byte))
+			Add-Content -Path $OutFile -Value "`t`t`"value`": `"$EncodedContents`",`n`t`t`"base64`": true`n`t},"
+		} else {
+			# Upload text files directly, since it's easier than escaping special characters in strings
+			if ($Preview.IsPresent) {
+				wrangler kv:key put $SubPath --binding $Binding --path $File.FullName --preview
+			} else {
+				wrangler kv:key put $SubPath --binding $Binding --path $File.FullName
+			}
+		}
+	} else {
+		Write-Host "File length is 0. Skipping."
+	}
+}
+# I'm lazy, so add a superfluous value rather than remove the comma from the last key
+Add-Content -Path $OutFile -Value "`t{`n`t`t`"key`": `"test`",`n`t`t`"value`": `"test`"`n`t}`n]"
+
+# Upload files in bulk
+Write-Host "Uploading bulk binary files:"
+if ($Preview.IsPresent) {
+	wrangler kv:bulk put $OutFile --binding $Binding --preview
+} else {
+	wrangler kv:bulk put $OutFile --binding $Binding
+}
